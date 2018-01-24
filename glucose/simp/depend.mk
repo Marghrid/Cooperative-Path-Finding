308e4e5e10b5c3771643e1af810331bfeb0b4805
Marghrid <marghrid@hotmail.com>
2018-01-24 07:46:43 +0000

    Glucose and minisat SAT solvers.

---
+++ glucose/simp/depend.mk
@@ -0,0 +1,40 @@
+/Users/ruicruzferreira/Downloads/glucose/simp/Main.o /Users/ruicruzferreira/Downloads/glucose/simp/Main.or /Users/ruicruzferreira/Downloads/glucose/simp/Main.od /Users/ruicruzferreira/Downloads/glucose/simp/Main.op: /Users/ruicruzferreira/Downloads/glucose/simp/Main.cc \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../utils/System.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/IntTypes.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../utils/ParseUtils.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../utils/Options.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Vec.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/XAlloc.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/Dimacs.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/SolverTypes.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Alg.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Map.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Alloc.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../simp/SimpSolver.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Queue.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/Solver.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Heap.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/BoundedQueue.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/Constants.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Clone.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/SolverStats.h
+/Users/ruicruzferreira/Downloads/glucose/simp/SimpSolver.o /Users/ruicruzferreira/Downloads/glucose/simp/SimpSolver.or /Users/ruicruzferreira/Downloads/glucose/simp/SimpSolver.od /Users/ruicruzferreira/Downloads/glucose/simp/SimpSolver.op: /Users/ruicruzferreira/Downloads/glucose/simp/SimpSolver.cc \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Sort.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Vec.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/IntTypes.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/XAlloc.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../simp/SimpSolver.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Queue.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/Solver.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Heap.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Alg.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../utils/Options.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../utils/ParseUtils.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/SolverTypes.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Map.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Alloc.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/BoundedQueue.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/Constants.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../mtl/Clone.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../core/SolverStats.h \
+  /Users/ruicruzferreira/Downloads/glucose/simp/../utils/System.h
