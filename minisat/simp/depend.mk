000ce032961a25b774b0a323da84ded5dba06149
Marghrid <marghrid@hotmail.com>
2018-01-24 07:46:43 +0000

    Glucose and minisat SAT solvers.

---
+++ minisat/simp/depend.mk
@@ -0,0 +1,32 @@
+/Users/ruicruzferreira/Downloads/minisat/simp/Main.o /Users/ruicruzferreira/Downloads/minisat/simp/Main.or /Users/ruicruzferreira/Downloads/minisat/simp/Main.od /Users/ruicruzferreira/Downloads/minisat/simp/Main.op: /Users/ruicruzferreira/Downloads/minisat/simp/Main.cc \
+  /Users/ruicruzferreira/Downloads/minisat/utils/System.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/IntTypes.h \
+  /Users/ruicruzferreira/Downloads/minisat/utils/ParseUtils.h \
+  /Users/ruicruzferreira/Downloads/minisat/utils/Options.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Vec.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/XAlloc.h \
+  /Users/ruicruzferreira/Downloads/minisat/core/Dimacs.h \
+  /Users/ruicruzferreira/Downloads/minisat/core/SolverTypes.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Alg.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Map.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Alloc.h \
+  /Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Queue.h \
+  /Users/ruicruzferreira/Downloads/minisat/core/Solver.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Heap.h
+/Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.o /Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.or /Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.od /Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.op: /Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.cc \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Sort.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Vec.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/IntTypes.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/XAlloc.h \
+  /Users/ruicruzferreira/Downloads/minisat/simp/SimpSolver.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Queue.h \
+  /Users/ruicruzferreira/Downloads/minisat/core/Solver.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Heap.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Alg.h \
+  /Users/ruicruzferreira/Downloads/minisat/utils/Options.h \
+  /Users/ruicruzferreira/Downloads/minisat/utils/ParseUtils.h \
+  /Users/ruicruzferreira/Downloads/minisat/core/SolverTypes.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Map.h \
+  /Users/ruicruzferreira/Downloads/minisat/mtl/Alloc.h \
+  /Users/ruicruzferreira/Downloads/minisat/utils/System.h
