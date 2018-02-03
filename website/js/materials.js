d1a697941be78a4dd9ef692370a948b723ddce7c
drcd1 <duartercorreia@hotmail.com>
2018-02-03 20:29:55 +0000

    Changed goal appearence

--- website/js/materials.js
+++ website/js/materials.js
@@ -4,4 +4,4 @@ let board_material_o = new THREE.MeshPhongMaterial({color:0x252525});
 
 // Materials for Agents3D and their Goals3D.
 let agent_material = new THREE.MeshPhongMaterial({color:0xff0000});
-let goal_material  = new THREE.MeshPhongMaterial({color:0xff0000, transparent: true, opacity: 0.4});
+let goal_material  = new THREE.MeshPhongMaterial({color:0xff0000, emissive:0xffffff, specular: 0x000000});
