8c16d9a109f60a97911715bb5e793eb03fce3c4d
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2018-02-05 13:27:58 +0000

    Script to move unsolved .cpfs out of intances/.

--- instance_cleaner.py
+++ instance_cleaner.py
@@ -5,14 +5,16 @@ import os
 cpfs = glob.glob("instances/*.cpf")
 outs = glob.glob("instances/*.out")
 
+croped_outs = []
 for filename in outs:
 	filename = re.sub('instances/', '', filename)
 	filename = re.sub('.out', '', filename)
+	croped_outs += [filename]
 
 
 for filename in cpfs:
 	filename = re.sub('instances/', '', filename)
 	filename = re.sub('.cpf', '', filename)
 
-	if filename not in outs:
+	if filename not in croped_outs:
 		os.rename("instances/" + filename + ".cpf", "unsolved_instances/" + filename + ".cpf")
