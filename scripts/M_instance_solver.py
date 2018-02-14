a64fdff54fd211707abc5bb83b5b9b6b69c8a5b3
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2018-02-14 07:33:06 +0000

    Script updated to solve hypergraph instances as well as grid.

--- scripts/M_instance_solver.py
+++ scripts/M_instance_solver.py
@@ -20,17 +20,16 @@ for size in (4, 8, 16, 32, 64):
 				print ("../CPFSolver/bin/a.out" + aux)
 				system("../CPFSolver/bin/a.out" + aux)
 
-#for dim in (3, 9):
-#	makespan_limit = 2**dim * 2
-#	for robots_prob in (0.1, 0.2, 0.4):
-#		aux = 2**dim * robots_prob
-#		n_robots = int(aux);
-#		for seed in (5, 31, 327, 616, 895):
-#			aux = " --input-file=../instances/hyper_dim" + str(dim) + "_a" + str(n_robots) + "_" + str(seed) + ".cpf"
-#			aux += " --output-file=../solutions/hyper_dim" + str(dim) + "_a" + str(n_robots) + "_" + str(seed) + ".out"
-#			aux += " --makespan-limit=" + str(makespan_limit)
-#			aux += " --strategy=binary"
-#
-#			output = " > ../solver_files/hyper_dim" + str(dim) + "_a" + str(n_robots) + "_" + str(seed) + ".txt 2>&1"
-#			print ("../reLOC-0.13-odaiba_037/src/solver_reLOC" + aux + output)
-#			system("../reLOC-0.13-odaiba_037/src/solver_reLOC" + aux + output)
+for dim in (3, 9):
+	makespan_limit = 2**dim * 2
+	for robots_prob in (0.1, 0.2, 0.4):
+		aux = 2**dim * robots_prob
+		n_robots = int(aux);
+		for seed in (5, 31, 327, 616, 895):
+			filename = "hyper_dim" + str(dim) + "_a" + str(n_robots) + "_" + str(seed)
+			aux  = " -i ../M_instances/"  + filename + ".cpf"
+			aux += " -o ../M_instances/" + filename + ".out"
+			aux += " -v 0"
+
+			print ("../CPFSolver/bin/a.out" + aux)
+			system("../CPFSolver/bin/a.out" + aux)
