c6caf9a43abf1abddf91067fd2a797b84c4df3c7
Marghrid <marghrid@hotmail.com>
2018-02-15 07:02:12 +0000

    Renamed some files, namely CPFSolver executable.

--- scripts/S_instance_solver.py
+++ scripts/S_instance_solver.py
@@ -9,17 +9,17 @@ output = " > ../solver_files/" + filename + ".txt 2>&1"
 print ("../reLOC-0.13-odaiba_037/src/solver_reLOC" + aux + output)
 system("../reLOC-0.13-odaiba_037/src/solver_reLOC" + aux + output)
 
-for size in (4, 8, 16, 32, 64):
-	makespan_limit = size * size *2
-	for robots_prob in (0.2, 0.3, 0.4):
-		for obst_prob in (0.1, 0.2, 0.3, 0.4):
+for size in (4, 8, 16, 32):
+	for robots_prob in (0.2, 0.3, 0.4, 0.6):
+		for obst_prob in (0.1, 0.2, 0.3):
 			n_robots = int(size * size * robots_prob);
+			makespan_limit = size * size * n_robots
 			for seed in (5, 31, 327, 616, 895):
 
-				filename  = "grid_" + str(size) + "x" + str(size)
-				filename += "_a" + str(n_robots)
+				filename  = "grid_" + str(size).zfill(2) + "x" + str(size).zfill(2)
+				filename += "_a" + str(n_robots).zfill(4)
 				filename += "_o" + str(obst_prob)
-				filename += "_s" + str(seed)
+				filename += "_s" + str(seed).zfill(3)
 
 				aux  = " --input-file=../instances/"  + filename + ".cpf"
 				aux += " --output-file=../instances/" + filename + ".out"
@@ -31,13 +31,14 @@ for size in (4, 8, 16, 32, 64):
 				system("../reLOC-0.13-odaiba_037/src/solver_reLOC" + aux + output)
 
 for dim in (3, 9):
-	makespan_limit = 2**dim * 2
 	for robots_prob in (0.1, 0.2, 0.4):
 		aux = 2**dim * robots_prob
 		n_robots = int(aux);
+		makespan_limit = 2**dim * n_robots
+
 		for seed in (5, 31, 327, 616, 895):
-			aux = " --input-file=../instances/hyper_dim" + str(dim) + "_a" + str(n_robots) + "_" + str(seed) + ".cpf"
-			aux += " --output-file=../solutions/hyper_dim" + str(dim) + "_a" + str(n_robots) + "_" + str(seed) + ".out"
+			aux = " --input-file=../instances/hyper_dim" + str(dim) + "_a" + str(n_robots).zfill(3) + "_" + str(seed).zfill(3) + ".cpf"
+			aux += " --output-file=../solutions/hyper_dim" + str(dim) + "_a" + str(n_robots).zfill(3) + "_" + str(seed).zfill(3) + ".out"
 			aux += " --makespan-limit=" + str(makespan_limit)
 			aux += " --strategy=binary"
 
