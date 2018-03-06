2a4cc711f32318668da32d2686634630b9d9fc0c
Marghrid <marghrid@hotmail.com>
2018-03-06 16:46:54 +0000

    update generate_command.py

--- scripts_improved/generate_command.py
+++ scripts_improved/generate_command.py
@@ -2,7 +2,8 @@ import constants
 
 
 def generate_command(instance_name, encoding="simplified", search="UNSAT-SAT", verbosity=0):
-	command = " -i ../handmade_instances/" + instance_name + ".cpf"
+	command  = "../CPFSolver/bin/CPFSolver"
+	command += " -i ../handmade_instances/" + instance_name + ".cpf"
 	command += " -o " + constants.solutions_dir  + instance_name + "_" + search + ".out"
 	command += " -s " + constants.stat_files_dir + instance_name + "_" + search + ".txt"
 	command += " -e " + encoding
