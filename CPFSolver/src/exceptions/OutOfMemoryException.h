983a4f1a211358f8cf6052b1bcf734e2ca3ad4e1
Marghrid <marghrid@hotmail.com>
2018-03-10 09:15:22 +0000

    Brand new CPFSolver custom exceptions.

---
+++ CPFSolver/src/exceptions/OutOfMemoryException.h
@@ -0,0 +1,14 @@
+//
+// Created by Rui Cruz Ferreira on 10/03/2018.
+//
+
+#ifndef CPFSOLVER_OUTOFMEMORYEXCEPTION_H
+#define CPFSOLVER_OUTOFMEMORYEXCEPTION_H
+
+
+class OutOfMemoryException {
+
+};
+
+
+#endif //CPFSOLVER_OUTOFMEMORYEXCEPTION_H
---
+++ CPFSolver/src/exceptions/TimeoutException.h
@@ -0,0 +1,14 @@
+//
+// Created by Rui Cruz Ferreira on 10/03/2018.
+//
+
+#ifndef CPFSOLVER_TIMEOUTEXCEPTION_H
+#define CPFSOLVER_TIMEOUTEXCEPTION_H
+
+
+class TimeoutException {
+
+};
+
+
+#endif //CPFSOLVER_TIMEOUTEXCEPTION_H
