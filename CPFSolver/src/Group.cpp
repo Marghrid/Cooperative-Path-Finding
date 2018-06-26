f45bba112ba681b7185c4710769f2f72de641ba5
Marghrid <marghrid@hotmail.com>
2018-06-26 15:11:32 +0100

    Solution using just one group with all agents.

---
+++ CPFSolver/src/Group.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Rui Cruz Ferreira on 26/06/2018.
+//
+
---
+++ CPFSolver/src/Group.h
@@ -0,0 +1,8 @@
+//
+// Created by Rui Cruz Ferreira on 26/06/2018.
+//
+
+#ifndef CPFSOLVER_GROUP_H
+#define CPFSOLVER_GROUP_H
+
+#endif //CPFSOLVER_GROUP_H
